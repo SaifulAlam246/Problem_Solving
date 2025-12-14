4bb712b6e940a62f933f33c7da76ab5fdc993923
saifulalam <saifulalamsaimon85@gmail.com>
2025-12-14 12:27:46 +0600

    string comparison problem

---
+++ Random_Problem_Practice/B_Comparison_String.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define nl '\n'
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve(){
+    int n;cin >> n;
+    string s;cin >> s;
+    int g=0,l=0,flag=1;
+    int mx=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='>'){
+            l=0;
+            g++;
+        }
+        else{
+            g=0;
+            l++;
+        }
+        mx=max({mx,g,l});
+    }
+    cout << mx+1 << nl;
+}
+
+
+int main()
+{
+  fast
+
+  int t; cin >> t;
+  while (t--)
+  {
+    solve();   
+  }
+
+  return 0;
+}
